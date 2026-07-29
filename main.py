967e46cc7762c07146d22b115ce0af32b958f8af
Sith <227094824+python3demon@users.noreply.github.com>
2026-07-29 20:32:33 +0300

    feat: Новый минималистичный UI, интеграция с проводником и хоткеи (v0.4.0-alpha) (#4)
    * feat: обновить дизайн интерфейса и добавить открытие файлов через проводник
    
    * refactor: привязать методы к self.current_file и добавить метод edit_note для перезаписи
    
    * feat: реализовать сохранение файлов по горячим клавишам Ctrl+S и диалог сохранения
    
    * feat: добавить индикатор имени файла в шапку с автообновлением при открытии и сохранении
    
    * docs: обновить дорожную карту в README под версию v0.4.0-alpha

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from tkinter import filedialog
 
 WINDOW_TITLE = "Note App"
 WINDOW_GEOMETRY = "600x400"
@@ -7,19 +8,39 @@ COLOR_THEME = "green"
 
 COLOR_SUCCESS = "green"
 COLOR_ERROR = "#c22121"
+BG_HEADER = "#212121"
+BG_TEXTAREA = "#0d0d0d"
+
+FILE_LABEL_FONT = "Courier New"
+FILE_LABEL_SIZE = 14
 
 class NoteManager:
-    def __init__(self):
-        self._notes: dict[str, str] = {"Hello": ""}
+    def __init__(self) -> None:
+        self.current_file = None
+        
+    def get_content(self) -> None:
+        with open(self.current_file, "r") as file:
+            file_content = file.read()
+        return file_content
 
-    def get_all_files(self):
-        return list(self._notes.keys())
-    
-    def get_content(self, filename: str):
-        return self._notes.get(filename)
-    
-    def save_note(self, filename: str, val: str):
-        self._notes[filename] = val
+    def edit_note(self, content) -> None:
+        with open(self.current_file, "w") as file:
+            file.write(content)
+
+    def open_file_dialog(self) -> str:
+        file_path = filedialog.askopenfilename(
+            title="Выберите файл",
+            filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")]
+        )
+        return file_path
+
+    def save_file_dialog(self) -> str:
+        file_path = filedialog.asksaveasfilename(
+            title="Сохранить файл как...",
+            defaultextension=".txt",
+            filetypes=[("Текстовые файлы", "*.txt"), ("Все файлы", "*.*")]
+        )
+        return file_path
     
 class EasyNoteApp(ctk.CTk):
     def __init__(self):
@@ -30,6 +51,8 @@ class EasyNoteApp(ctk.CTk):
         ctk.set_appearance_mode(APPEARANCE_MODE)
         ctk.set_default_color_theme(COLOR_THEME)
 
+        self.bind("<Control-s>", self._save_file)
+
         self._manager = NoteManager()
 
         self._configure_grid()
@@ -37,61 +60,58 @@ class EasyNoteApp(ctk.CTk):
 
     def _configure_grid(self) -> None:
         self.grid_columnconfigure(0, weight=1)
-        self.grid_columnconfigure(1, weight=0)
-        self.grid_columnconfigure(2, weight=0)
+        self.grid_rowconfigure(0, weight=0)
         self.grid_rowconfigure(1, weight=1)
 
     def _create_widgets(self) -> None:
-        self._title_entry = ctk.CTkEntry(self, placeholder_text="Введите название файла...")
-        self._menu_options = ctk.CTkOptionMenu(master=self, values=self._manager.get_all_files(), command=self._on_select_note)
-
-        self._text_area = ctk.CTkTextbox(self)
-        self._status_label = ctk.CTkLabel(self, text="")
-
-        self._save_button, self._clear_button = (
-            ctk.CTkButton(self, text="Сохранить", command=self._save_note),
-            ctk.CTkButton(self, text="Очистить", command=self._clear_text),
+        self._header_frame = ctk.CTkFrame(self, height=48, corner_radius=0, fg_color=BG_HEADER)
+        self._header_frame.grid_columnconfigure(0, weight=0)
+        self._header_frame.grid_columnconfigure(1, weight=1)
+        self._header_frame.grid(row=0, column=0, sticky="ew")
+        
+        self._open_file_button = ctk.CTkButton(
+            self._header_frame,
+            text="Open",
+            command=self._open_file,
+            width=72,
+            fg_color="transparent"
         )
+        self._open_file_button.grid(row=0, column=0, padx=12, pady=4)
 
-        # Загаловок, текстовое поле, строка состояние
-        self._title_entry.grid(row=0, column=0, padx=15, pady=15, sticky="ew", columnspan=2)
-        self._menu_options.grid(row=0, column=2, padx=15, pady=15, sticky="e")
-        self._text_area.grid(row=1, column=0, padx=15, pady=5, sticky="nsew", columnspan=3)
-        self._status_label.grid(row=2, column=0, padx=15, pady=5, sticky="ew", columnspan=3)
+        self._file_label = ctk.CTkLabel(
+            self._header_frame,
+            text="• Новый файл",
+            font=(FILE_LABEL_FONT, FILE_LABEL_SIZE, "bold")
+        )
+        self._file_label.grid(row=0, column=1, padx=28, pady=4, sticky="e")
 
-        # Кнопки
-        self._save_button.grid(row=3, column=0, columnspan=2, sticky="ew", padx=15, pady=15)
-        self._clear_button.grid(row=3, column=2, sticky="ew", padx=15, pady=15)
+        self._text_area = ctk.CTkTextbox(self, corner_radius=0, fg_color=BG_TEXTAREA)
+        self._text_area.grid(row=1, column=0, sticky="nsew")
 
-    def _save_note(self) -> None:
-        title = self._title_entry.get().strip()
-        if len(title) < 3:
-            self._update_status(
-                text="Имя файла должно быть не короче 3 символов!",
-                text_color=COLOR_ERROR,
-            )
+    def _open_file(self, choice: str = "Hello") -> None:
+        file_path = self._manager.open_file_dialog()
+        if not file_path:
             return
+        self._manager.current_file = file_path
+        file_content = self._manager.get_content()
 
-        content = self._text_area.get("1.0", "end-1c")
-        self._manager.save_note(title, content)
-        self._menu_options.configure(values=self._manager.get_all_files())
-        self._menu_options.set(title)
-
-        self._update_status(text="Файл успешно сохранен!", text_color=COLOR_SUCCESS)
-
+        self._clear_text()
+        self._file_label.configure(text=f"{file_path.split('/')[-1]}")
+        self._text_area.insert("1.0", file_content)
+        self._manager.current_file = file_path
+    
     def _clear_text(self) -> None:
         self._text_area.delete("1.0", "end")
 
-    def _on_select_note(self, choice: str) -> None:
-        self._title_entry.delete(0, "end")
-        self._title_entry.insert(0, choice)
-
-        self._text_area.delete("1.0", "end")
-        self._text_area.insert("1.0", self._manager.get_content(choice))
-
-    def _update_status(self, text: str, text_color: str) -> None:
-        self._status_label.configure(text=text, text_color=text_color)
-
+    def _save_file(self, event=None) -> None:
+        if self._manager.current_file is None:
+            file_path = self._manager.save_file_dialog()
+            if not file_path: return
+            self._manager.current_file = file_path
+            self._file_label.configure(text=f"{file_path.split('/')[-1]}")
+            
+        new_content = self._text_area.get("1.0", "end")
+        self._manager.edit_note(new_content)
 
 if __name__ == "__main__":
     app = EasyNoteApp()
